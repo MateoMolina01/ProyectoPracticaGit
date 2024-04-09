9a5561e25ea4fd1a7ef834e01af0f77fca4109f8
Mokka <mmolinac20@educastillalamancha.es>
2024-04-09 18:22:00 +0200

    Segundo Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(FUtil.existe("F:\\GRADO\\PROGRAMACIÓN\\Intelli\\ProyectoPracticaGit\\src\\ClasePrueba.java"));
+        System.out.println(FUtil.existe("F:\\GRADO\\PROGRAMACIÓN\\Intelli\\ProyectoPracticaGit\\src\\FUtil.java"));
+        System.out.println(FUtil.existe("F:\\GRADO\\PROGRAMACIÓN\\Intelli\\ProyectoPracticaGit\\src\\Main.java"));
     }
 }
